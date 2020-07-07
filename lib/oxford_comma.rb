94b920dbde46ff1b46468e0804f5b238419b6f47
Michael Ciabuca <ciabuca@gmail.com>
2020-07-07 19:54:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,5 @@
 def oxford_comma(array)
-  if(array.size == 2)
-    array.join(" and ")
-  else
-    array.join(', ')
-  end
+  array.join(' and ') if array.size < 3
+  array[-1] = "and " + array[-1]
+  array.join(', ')
 end
